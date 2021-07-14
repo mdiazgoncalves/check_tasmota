3898a864751690e7bdf115f78907867be1ac56ae
Miguel Diaz Gonçalves <mcdg@outlook.pt>
2021-07-14 16:03:35 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,64 @@
 # check_tasmota
-Nagios plugin to check availability of tasmota devices
+Nagios plugin to check availability of Tasmota devices
+
+
+To install Requests, simply run this simple command in your terminal of choice:
+
+python -m pip install requests
+
+
+
+
+
+# check_tasmota
+
+
+Nagios plugin to check availability of [Tasmota](https://tasmota.github.io/docs/) devices. Checks if the ESPHome device is online
+
+## Installation
+
+The installation requires Python 3.
+
+I usually install additional plugins under `/usr/local/nagios/libexec`.
+
+```
+mkdir -p /usr/local/nagios/libexec
+cd /usr/local/nagios/libexec
+wget https://raw.githubusercontent.com/mdiazgoncalves/check_tasmota/main/check_tasmota.py
+chmod +x check_tasmota.py
+```
+
+The [requests](https://github.com/psf/requests) module is required.
+
+It can be installed using:
+
+```
+pip3 install requests
+```
+
+## Parameters
+
+```
+usage: check_tasmota.py [-h] [-U username] [-P password] <hostname>
+
+Check Tasmota node availability
+
+positional arguments:
+  <hostname>            The hostname/ip of the device
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -U username, --username username
+                        the tasmota api username
+  -P password, --password password
+                        the tasmota api password
+
+```
+
+## Support
+
+Feel free to submit any issues and PR's'.
+
+## License
+
+The project is licensed under GPL license. Happy monitoring.
--- check_tasmota.py
+++ check_tasmota.py
@@ -1,45 +1,36 @@
 #!/usr/bin/python3
 
-# check_tasmota
+# https://nagios-plugins.org/doc/guidelines.html
 
 # Import required libs for your plugin
 import argparse
 import requests
-from urllib.parse import quote
 from requests.auth import HTTPBasicAuth
 
 # Return codes expected by Nagios
 codes = [ 'OK', 'WARNING', 'CRITICAL', 'UNKNOWN' ]
 
 # Create the parser
-my_parser = argparse.ArgumentParser(description='Check Tasmota node')
+my_parser = argparse.ArgumentParser(description='Check Tasmota node availability')
 
 # Add the arguments
-my_parser.add_argument('Address', metavar='address', type=str, help='the host ip address')
+my_parser.add_argument('hostname', metavar='<hostname>', type=str, help='The hostname/ip of the device')
 my_parser.add_argument('-U', "--username", metavar='username', type=str, help='the tasmota api username')
 my_parser.add_argument('-P', "--password", metavar='password', type=str, help='the tasmota api password')
 
 # Execute the parse_args() method
 args = my_parser.parse_args()
 
-payload = {'cmnd': 'Status%200' }
-
-# if args.username:
-#     payload['user']= args.username
-#     payload['password']= quote(args.password)
-
 # Check logic starts here
 
 try:
-    response = requests.get('http://' + args.Address + '/cm', auth=HTTPBasicAuth(args.username, args.password))
-    print(response.text)
+    response = requests.get('http://' + args.hostname + '/cm?cmnd=Status%200', auth=HTTPBasicAuth(args.username, args.password))
     response.raise_for_status()
-    data = response.json()
-    print(data)
 except requests.exceptions.RequestException  as e:
     status = 2
     message =str(e)
 else:
+    data = response.json()
     status = 0
     message = "name:{} mac_address:{} version:{}".format(data['Status']['DeviceName'],data['StatusNET']['Mac'],data['StatusFWR']['Version'])
 
